d05b0382dfb86a7e9bc42843d13b628bb1188c68
mahdi <mahdighasri@gmail.com>
2020-01-03 20:24:53 +0330

    add windows service sample. This is worker service sample as windows service.

---
+++ src/TestWindowsService/Worker1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TestWindowsService
+{
+    public class Worker1 : BackgroundService
+    {
+        private readonly ILogger<Worker1> _logger;
+
+        public Worker1(ILogger<Worker1> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker one running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1500, stoppingToken);
+            }
+        }
+    }
+}
